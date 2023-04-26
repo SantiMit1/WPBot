7fe3462effe29ebcce2f64957045d6efe4adf21f
Santiago Mitidiero <santimiti10@gmail.com>
2023-04-26 15:34:38 -0300

    sticker bot

---
+++ index.js
@@ -0,0 +1,30 @@
+const qrcode = require("qrcode-terminal");
+const { Client, MessageMedia } = require("whatsapp-web.js");
+
+const client = new Client({
+  puppeteer: {
+    args: ["--no-sandbox"],
+  },
+});
+
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on("ready", () => {
+  console.log("Client is ready!");
+});
+
+client.on("message_create", async (msg) => {
+  if (msg.body == "!s" && msg.hasMedia) {
+    const media = await msg.downloadMedia();
+    const sticker = new MessageMedia(media.mimetype, media.data);
+    client.sendMessage(msg.from, sticker, {
+      sendMediaAsSticker: true,
+      stickerAuthor: "--",
+      stickerName: "--",
+    });
+  }
+});
+
+client.initialize();
